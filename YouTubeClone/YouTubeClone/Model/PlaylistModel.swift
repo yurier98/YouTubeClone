4d087f70f9c2dd4d900daeeb1440ceaeeafa7bdc
Yurier Herrera <yurierjesus@gmail.com>
2023-08-26 11:23:40 -0400

    first commit

---
+++ YouTubeClone/YouTubeClone/Model/PlaylistModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlaylistModel.swift
+//  YouTubeClone
+//
+//  Created by Yurier  Herrera on 24/8/23.
+//
+
+import Foundation
---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Yurier  Herrera on 16/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
